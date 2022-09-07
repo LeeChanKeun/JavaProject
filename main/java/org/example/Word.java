03370569940a1fd922247e4fc1b9fd03c665e767
leechankeun <21900575@handong.ac.kr>
2022-09-07 15:18:18 +0900

    project1

--- main/java/org/example/Word.java
+++ main/java/org/example/Word.java
@@ -3,15 +3,13 @@ package org.example;
 public class Word {
     private String name;
 
-
-
-
-
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 }
