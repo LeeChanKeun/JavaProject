d0c8464498c7d48b32d750f5d21c5c397dd2c7a7
leechankeun <21900575@handong.ac.kr>
2022-09-07 21:27:15 +0900

    project1

--- main/java/org/example/Word.java
+++ main/java/org/example/Word.java
@@ -1,15 +1,23 @@
 package org.example;
 
 public class Word {
-    private String name;
-
-    public String getName() {
-
-        return name;
+    private int id;
+    private int level;
+    private String meaning;
+    private String word;
+    Word(int id, int level, String word, String meaning){
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
     }
 
-    public void setName(String name) {
 
-        this.name = name;
+
+    public String getName() {
+        return word;
+    }
+    public void setName(String name) {
+        this.word = name;
     }
 }
