168a2ccec9c87c9a4d208d7646395f7eca41e518
leechankeun <21900575@handong.ac.kr>
2022-09-07 15:18:53 +0900

    project1

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        //what
+        //what is wrong
         System.out.println("Hello world!");
     }
 }
