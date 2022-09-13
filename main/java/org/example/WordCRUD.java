a3cc69f728c07de0cf00e4337fd148a2ed357242
leechankeun <21900575@handong.ac.kr>
2022-09-13 22:10:41 +0900

    project1

--- main/java/org/example/WordCRUD.java
+++ main/java/org/example/WordCRUD.java
@@ -1,9 +1,6 @@
 package org.example;
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class WordCRUD implements ICRUD {
@@ -127,6 +124,15 @@ public class WordCRUD implements ICRUD {
     }
 
     public void saveFile() {
-
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter("test.txt"));
+            for(Word one : list){
+                pr.write(one.toFileString() + "\n");
+            }
+            pr.close();
+            System.out.println("==> 데이터 저장 완료 !!!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
